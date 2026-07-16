2e584aefda4236744ba90692726d2d448402cc5c
twizzcode <twizzcode@gmail.com>
2026-07-16 10:32:22 +0700

    umami

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -28,6 +28,11 @@ export function GoogleAnalytics() {
           `,
         }}
       />
+      <Script
+        strategy="afterInteractive"
+        src="https://umami-twizz.netlify.app/script.js"
+        data-website-id="236b6645-7a5b-4ca3-a0d7-4dce87b06f9c"
+      />
     </>
   )
 }
